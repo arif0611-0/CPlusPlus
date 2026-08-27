e6c4267b7cf67b987617ec683a9bef9135b264bd
arif0611-0 <ariannafakhri@hotmail.com>
2026-08-27 14:27:10 -0700

    Added Fictional Person file completed

---
+++ FictionalPerson.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : FictionalPerson.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Bambi" << endl;
+	cout << "Balles" << endl;
+	cout << "1390 71st ave se" << endl;
+	cout << "Auburn" << endl;
+	cout << "98092" << endl;
+	return 0;
+}
