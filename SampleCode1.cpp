fc606b52ee2905c4225dc8707fa384ecb72867f3
arif0611-0 <ariannafakhri@hotmail.com>
2026-08-27 14:51:22 -0700

    Add files via upload

---
+++ SampleCode1.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : SampleCode1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include<iostream>
+
+
+//Standard namespace declaration
+using namespace std;
+
+//Main Function
+int main()
+{
+//Standard Output Statement
+cout << "Welcome to this C++ Program" << endl;
+cout << "I have corrected all errors for this program." << endl;
+// Wait For Output Screen
+//Main Function return Statement
+return 0;
+}
